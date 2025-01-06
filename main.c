db9ed65542b342cd8dffb23e4094a4a16df17d6c
djumanov <djumanovdev@gmail.com>
2025-01-06 16:41:09 +0500

    read file

--- main.c
+++ main.c
@@ -2,5 +2,21 @@
 
 int main(){
 
+    FILE *f = fopen("data.txt", "r");
+
+    char word1[100], word2[100];
+
+    fscanf(f, "%s", word1);
+    puts(word1);
+
+    fscanf(f, "%s", word2);
+    puts(word2);
+    
+    if (feof(f)) {
+        printf("stream is done..\n");
+    } else {
+        printf("Stream is not done..\n");
+    }
+
     return 0;
 }
